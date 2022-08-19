85f236f41050b8e82cf4ed469b94d126e6f59ed2
juanse <ladinomendietajuanse@gmail.com>
2022-08-18 21:23:10 -0500

    creacion_tabals_db

--- app/model/users.py
+++ app/model/users.py
@@ -1,3 +1,39 @@
-from sqlalchemy import Table, Column
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, VARCHAR, TIMESTAMP, BOOLEAN, String, TEXT
 from config.db import engine, meta_data
+
+suscription = Table(
+    'suscription', meta_data, 
+    Column('id', Integer, primary_key =True),
+    Column('suscription_Type', VARCHAR(255), nullable=False),
+)
+
+usertype = Table(
+    'usertype', meta_data,
+    Column('id', Integer, primary_key =True),
+    Column('user_Type', VARCHAR(255), nullable=False),
+)
+
+user_base = Table(
+    'user_base', meta_data,
+    Column('id', Integer, primary_key =True),
+    Column('name', VARCHAR(255), nullable=False),
+    Column('last_name', VARCHAR(255), nullable=False),
+    Column('password', String(255), nullable=False),
+    Column('payment', Integer), 
+    Column('suscription_id', Integer, ForeignKey('suscription.id')),
+    Column('user_type_id', Integer, ForeignKey('usertype.id')),
+)
+
+process = Table(
+    'user_base', meta_data,
+    Column('id', Integer, primary_key =True),
+    Column('tittle', VARCHAR(255), nullable=False),
+    Column('description', TEXT, nullable=False),
+    Column('start date', TIMESTAMP),
+    Column('finish date', TIMESTAMP),
+    Column('status', BOOLEAN),
+    Column('user_base_id', Integer, ForeignKey('user_base.id'))   
+)
+
+meta_data.create_all(engine)
