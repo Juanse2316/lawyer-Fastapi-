e45ad04c488862a35708db458cf845cb0707bfa2
juanse <ladinomendietajuanse@gmail.com>
2022-10-17 19:57:10 -0500

    change engine conection on local host database to cloud database

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:toor@localhost:3306/fastapi")
+# engine = create_engine("mysql+pymysql://root:toor@localhost:3306/fastapi")
+engine = create_engine("mysql+pymysql://sql10527362:5BEpYlfghy@sql10.freesqldatabase.com:3306/sql10527362")
 
 conn = engine.connect()
 
