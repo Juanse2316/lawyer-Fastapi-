0ef6232769c411aef34d0b2774b3c8ffecdf3eff
juanse <ladinomendietajuanse@gmail.com>
2022-10-25 16:43:30 -0500

    dando permisos

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from fastapi import FastAPI
 from router.router import user
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user)
