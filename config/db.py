a066de1eef88f145b7ecfd14a6e4be8a201414f6
Juan Ladino <ladinomendietajuanse@gmail.com>
2022-10-28 16:43:51 -0500

    Update the phyton version

--- config/db.py
+++ config/db.py
@@ -1,8 +1,38 @@
-from sqlalchemy import create_engine, MetaData
+from google.cloud.sql.connector import Connector
+import google.auth
+from google.auth.transport.requests import Request
+import sqlalchemy
+from sqlalchemy import MetaData
 
-# engine = create_engine("mysql+pymysql://root:toor@localhost:3306/fastapi")
-engine = create_engine("mysql+pymysql://sql10527362:5BEpYlfghy@sql10.freesqldatabase.com:3306/sql10527362")
+# IAM database user parameter (IAM user's email before the "@" sign, mysql truncates usernames)
+# ex. IAM user with email "demo-user@test.com" would have database user name "demo-user"
+IAM_USER = 'ladinomendietajuanse'[0].split("@")[0]
 
-conn = engine.connect()
+# get application default credentials of IAM user (current logged in user)
+credentials, project = google.auth.default()
 
+# refresh credentials if expired
+if not credentials.valid:
+      request = Request()
+      credentials.refresh(request)
+
+# initialize connector
+connector = Connector()
+
+# getconn now using IAM user and OAuth2 token as password
+def getconn():
+    conn = connector.connect(
+      'fastapi-lawyer-project1:us-central1:fastapi-lawyer1',
+      "pymysql",
+      user=IAM_USER,
+      password=credentials.token,
+      db="", # log in to instance but don't connect to specific database
+    )
+    return conn
+
+# create connection pool
+pool = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconn,
+)
 meta_data = MetaData()
